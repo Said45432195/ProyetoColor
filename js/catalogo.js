78b3dd201c9b3bd3bed79515204fb94a763e9c57
Said45432195 <said45432195@gmail.com>
2026-04-03 22:16:26 -0500

    Initial commit

---
+++ js/catalogo.js
@@ -0,0 +1,23 @@
+
+const productos = [
+    // --- INDUSTRIAL ---
+    { sku: "IND-AV-001", cat: "Industrial", sub: "Alta Visibilidad", marca: "Colorbang", tit: "Parka Reflectante Clase III", precio: "$42.500", tallas: ["S", "M", "L", "XL"], specs: ["Norma ISO 20471", "Impermeable", "Cintas 3M"], msg: "Cotizo Parka AV 001" },
+    { sku: "IND-CA-001", cat: "Industrial", sub: "Calzado", marca: "ProStep", tit: "Botín de Seguridad Pro", precio: "$35.000", tallas: ["38", "40", "42", "44"], specs: ["Punta Acero", "Antideslizante"], msg: "Cotizo Botín 001" },
+    { sku: "IND-TE-001", cat: "Industrial", sub: "Térmico", marca: "Colorbang", tit: "Chaqueta Térmica Minera", precio: "$28.990", tallas: ["M", "L", "XL"], specs: ["Interior Polar", "Repelente al agua"], msg: "Cotizo Chaqueta Térmica" },
+    { sku: "IND-MI-001", cat: "Industrial", sub: "Minería", tit: "Overol Trabajo Pesado", precio: "$24.500", tallas: ["S", "M", "L", "XL"], specs: ["Refuerzo en rodillas", "Tela Poplin"], msg: "Cotizo Overol 001" },
+    { sku: "IND-GU-001", cat: "Industrial", sub: "Guardia", tit: "Casaca Térmica Guardia", precio: "$32.000", tallas: ["M", "L", "XL"], specs: ["Acolchado térmico", "Color Negro"], msg: "Cotizo Casaca Guardia" },
+
+    // --- INSTITUCIONAL ---
+    { sku: "INS-LI-001", cat: "Institucional", sub: "Oficina", marca: "Colorbang", tit: "Camisa Oxford Premium", precio: "$18.990", tallas: ["S", "M", "L"], specs: ["Algodón/Poliéster", "Fácil planchado"], msg: "Cotizo Camisa Oxford" },
+    { sku: "INS-GU-001", cat: "Institucional", sub: "Guardia", tit: "Sweater Cuello V", precio: "$15.500", tallas: ["S", "M", "L", "XL"], specs: ["Tejido acrílico", "Sin pelusas"], msg: "Cotizo Sweater Guardia" },
+    { sku: "INS-SA-001", cat: "Institucional", sub: "Salud", tit: "Cotona Médica Blanca", precio: "$12.990", tallas: ["S", "M", "L"], specs: ["Tela Trevira", "Tres bolsillos"], msg: "Cotizo Cotona 001" },
+    { sku: "INS-PO-001", cat: "Institucional", sub: "Corporativo", tit: "Polerón Polar Micro", precio: "$14.000", tallas: ["S", "M", "L", "XL"], specs: ["Antipilling", "Cierre corto"], msg: "Cotizo Polar 001" },
+    { sku: "INS-PA-001", cat: "Institucional", sub: "Corporativo", tit: "Pantalón Vestir Hombre", precio: "$22.500", tallas: ["42", "44", "46", "48"], specs: ["Corte clásico", "Tela gabardina"], msg: "Cotizo Pantalón Vestir" },
+
+    // --- OUTDOOR ---
+    { sku: "OUT-TE-001", cat: "Outdoor", sub: "Térmico", marca: "Izmot", tit: "Chaqueta Softshell Pro", precio: "$38.000", tallas: ["S", "M", "L", "XL"], specs: ["Resistente al viento", "Bolsillos sellados"], msg: "Cotizo Softshell Pro" },
+    { sku: "OUT-MI-001", cat: "Outdoor", sub: "Terreno", tit: "Bota Trekking Impermeable", precio: "$45.000", tallas: ["39", "40", "41", "42"], specs: ["Membrana Gore-tex", "Suela Vibram"], msg: "Cotizo Bota Trekking" },
+    { sku: "OUT-CO-001", cat: "Outdoor", sub: "Viento", tit: "Cortaviento Ligero", precio: "$19.990", tallas: ["M", "L", "XL"], specs: ["Tela Ripstop", "Capucha oculta"], msg: "Cotizo Cortaviento 001" },
+    { sku: "OUT-CA-001", cat: "Outdoor", sub: "Terreno", tit: "Pantalón Cargo Táctico", precio: "$26.000", tallas: ["42", "44", "46"], specs: ["Costuras reforzadas", "Multibolsillo"], msg: "Cotizo Pantalón Cargo" },
+    { sku: "OUT-CZ-001", cat: "Outdoor", sub: "Accesorio", tit: "Calcetín Térmico Lana", precio: "$4.500", tallas: ["Estándar"], specs: ["Lana Merino", "Sin costuras"], msg: "Cotizo Calcetín Térmico" }
+];
